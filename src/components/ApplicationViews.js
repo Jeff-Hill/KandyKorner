3483e145df4fd78addbbf36af50fa134e2631490
Jeff Hill <jeffhill1978@gmail.com>
2019-07-15 15:28:48 -0500

    refactored to use json database and API fetch call

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -7,41 +7,29 @@ import CandyList from "./candy/CandyList"
 
 export default class ApplicationViews extends Component {
 
-candyLocationsFromAPI = [
-{ id: 1, name: "Kandy Store 1", address: "123 Candy Street"},
-{ id: 2, name: "Kandy Store 2", address: "456 S. Candy Ave"},
-{ id: 3, name: "Kandy Store 3", address: "789 N. Candy Blvd"}
-]
-
-employeesFromAPI = [
-    {id: 1, name: "Jonathan Sugaaddict"},
-    {id: 2, name: "Walter Sweettooth"},
-    {id: 3, name: "Amy Cavity"}
-]
-
-candyTypesFromAPI = [
-    {id: 1, name: "Licorice"},
-    {id: 2, name: "Candy Bars"},
-    {id: 3, name: "Gummies"},
-    {id: 4, name: "Hard Candies"}
-]
-
-individualCandiesFromAPI = [
-    {id: 1, candyTypeId: 1, name: "Strawberry Twizzlers"},
-    {id: 2, candyTypeId: 1, name: "Black Licorice"},
-    {id: 3, candyTypeId: 2, name: "Snickers"},
-    {id: 4, candyTypeId: 2, name: "Peanut Butter Twix"},
-    {id: 5, candyTypeId: 3, name: "Hariboo Gummy Bears"},
-    {id: 6, candyTypeId: 3, name: "Gummy Worms"},
-    {id: 7, candyTypeId: 4, name: "Peppermints"},
-    {id: 8, candyTypeId: 4, name: "Butter Scotch"}
-]
-
 state = {
-    employees: this.employeesFromAPI,
-    locations: this.candyLocationsFromAPI,
-    candyTypes: this.candyTypesFromAPI,
-    candies: this.individualCandiesFromAPI
+    employees: [],
+    locations: [],
+    candyTypes: [],
+    candies: []
+}
+
+componentDidMount() {
+    const newState = {}
+
+    fetch("http://localhost:5002/locations")
+        .then(r => r.json())
+        .then(locations => newState.locations = locations)
+        .then(() => fetch("http://localhost:5002/employees")
+        .then(r => r.json()))
+        .then(employees => newState.employees = employees)
+        .then(() => fetch("http://localhost:5002/candies")
+        .then(r => r.json()))
+        .then(candies => newState.candies = candies)
+        .then(() => fetch("http://localhost:5002/candyTypes")
+        .then(r => r.json()))
+        .then(candyTypes => newState.candyTypes = candyTypes)
+        .then(() => this.setState(newState))
 }
 
 render() {
